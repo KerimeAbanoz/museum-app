ff736752e4ad583db1bc3ace4988ea6951fd93fc
kerime abanoz <krmeabnz@gmail.com>
2023-03-07 11:23:02 +0300

    some progress

--- src/components/ArtCard.jsx
+++ src/components/ArtCard.jsx
@@ -1,52 +1,33 @@
-import React, { useContext } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { AuthContext } from "../context/AuthContextProvider";
-// import { toastWarnNotify } from "../helpers/ToastNotify";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const IMG_API = "https://image.tmdb.org/t/p/w1280";
+// const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
-const ArtCard = () => {
-//   const { currentUser } = useContext(AuthContext);
-//   const navigate = useNavigate();
+const ArtCard = ({
+  accessionNumber,
+  artistDisplayName,
+  additionalImages,
+  department,
+  objectDate,
+  objectID,
+  dimensions,
+  accessionYear,
+  title,
+}) => {
+  const navigate = useNavigate();
 
-//   const getVoteClass = (vote) => {
-//     if (vote >= 8) {
-//       return "green";
-//     } else if (vote >= 6) {
-//       return "orange";
-//     } else {
-//       return "red";
-//     }
-//   };
   return (
-    <div></div>
-    // <div
-    //   className="movie"
-    //   onClick={() => {
-    //     navigate("details/" + id);
-    //     !currentUser && toastWarnNotify("please log in to see details");
-    //   }}
-    // >
-    //   <img
-    //     loading="lazy"
-    //     src={poster_path ? IMG_API + poster_path : defaultImage}
-    //     alt="movie-card"
-    //   />
-    //   <div className="flex align-baseline justify-between p-1 text-white">
-    //     <h5>{title}</h5>
-    //     {currentUser && (
-    //       <span className={`tag ${getVoteClass(vote_average)}`}>
-    //         {vote_average.toFixed(1)}
-    //       </span>
-    //     )}
-    //   </div>
-    //   <div className="movie-over">
-    //     <h2>Overview</h2>
-    //     <p>{overview}</p>
-    //   </div>
-    // </div>
+    <div className="movie" onClick={() => navigate("details/" + objectID)}>
+      <h1>{title}</h1>
+      <h2>{dimensions}</h2>
+      <img
+        loading="lazy"
+        src={additionalImages ? additionalImages : defaultImage}
+        alt="art-work"
+      />
+    </div>
   );
 };
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,68 +1,130 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// // import ArtCard from "../components/ArtCard";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ArtCard from "../components/ArtCard";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import SearchIcon from "@mui/icons-material/Search";
 
-// const API_KEY = "https://collectionapi.metmuseum.org/public/collection/v1/objects";
+const API = "https://collectionapi.metmuseum.org/public/collection/v1/objects";
 
-// const Main = () => {
-
-//     const [movies, setMovies] = useState([]);
-//     const [searchTerm, setSearchTerm] = useState("");
-//     const [loading, setLoading] = useState(false);
-  
-//     useEffect(() => {
-//       getMovies(API_KEY);
-//     }, []);
-  
-//     const getMovies = (API) => {
-//       setLoading(true);
-//       axios
-//         .get(API)
-//         .then((res) => setMovies(res.data.results))
-//         .catch((err) => console.log(err))
-//         .finally(() => setLoading(false));
-//     };
-  
-
-
-//   return (
-//     <>
-//       <form className="flex justify-center p-2">
-//         <input
-//           type="search"
-//           className="w-80 h-8 rounded-md outline-none border p-1 m-2"
-//           placeholder="Search a movie..."
-//           onChange={(e) => setSearchTerm(e.target.value)}
-//           value={searchTerm}
-//         />
-//         <button className="dark:text-white" type="submit">
-//           Search
-//         </button>
-//       </form>
-//       <div className="flex justify-center flex-wrap">
-//         {/* {loading ? (
-//           <div
-//             className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
-//             role="status"
-//           >
-//             <span className="visually-hidden">Loading...</span>
-//           </div>
-//         ) : (
-//           movies.map((movie) => <ArtCard key={movie.id} {...movie} />)
-//         )} */}
-//       </div>
-//     </>
-//   )
-// }
-
-// export default Main
-
-import React from 'react'
+// const denemearray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const denemearray = [457, 4656, 5678, 567, 123, 345, 57, 7890, 7756];
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  // const [artWorks, setArtWorks] = useState([]);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default Main
+  // function getArtWorks() {
+  //   setLoading(true);
+
+  //   axios
+  //     .get(API)
+  //     .then((res) => {
+  //       console.log(res?.data?.objectIDs);
+  //       setArtWorks(res?.data?.objectIDs);
+  //       setData((data) => [...data, res.data]);
+  //       // getSpecificArtWork();
+  //       setLoading(false);
+  //     })
+  //     // .then(artWorks.slice(0, 10).forEach((id) => getSpecificArtWork(id)))
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  //   setLoading(false);
+  // }
+
+  const getSpecificArtWork = async () => {
+    setLoading(true);
+    denemearray.forEach((id) =>
+      axios
+        .get(API + "/" + id)
+        .then((res) => {
+          console.log(res.data);
+          setData((data) => [...data, res.data]);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    );
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    // getArtWorks(API);
+    getSpecificArtWork();
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
+
+  if (loading) {
+    console.log(loading);
+    // return <h1>HELLO</h1>;
+  }
+
+  return (
+    <>
+      <form
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "0.5rem",
+        }}
+      >
+        <input
+          type="search"
+          placeholder="Search"
+          style={{
+            width: "20rem",
+            height: "2rem",
+            borderRadius: "6px",
+            outline: "2px solid transparent",
+            padding: "4px",
+            margin: "0.5rem",
+          }}
+        />
+        <SearchIcon
+          style={{ fontSize: "2rem", margin: "1rem", color: "white" }}
+        />
+        <button
+          type="submit"
+          style={{
+            color: "white",
+            backgroundColor: "transparent",
+            border: "none",
+          }}
+        >
+          Search
+        </button>
+        <FilterAltIcon
+          style={{ fontSize: "2rem", margin: "1rem", color: "white" }}
+        />
+        <button
+          type="submit"
+          style={{
+            color: "white",
+            backgroundColor: "transparent",
+            border: "none",
+          }}
+        >
+          Filter
+        </button>
+      </form>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {data?.map((work) => (
+          <ArtCard key={work.id} {...work} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Main;
